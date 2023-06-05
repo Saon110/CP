3553c5e117ef67b1a9125a0935696c1eb7c1a8f3
Sijon Chisty Saon <2105110.ugrad.cse.buet.ac.bd>
2023-06-06 01:20:55 +0600

    Created a git repository to upload my codes
    This repo is for CP

---
+++ Codeforces/155A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ap=0;
+    for(int i=1;i<n;i++){
+
+        int k=0;
+
+        for(int j=0;j<i;j++){
+            
+           if(arr[j]<arr[i])k++;
+        }
+
+        if(k==i)ap++;
+    }
+
+    for(int i=1;i<n;i++){
+
+        int k=0;
+
+        for(int j=0;j<i;j++){
+            
+            if(arr[j]>arr[i])k++;
+        }
+
+        if(k==i)ap++;
+    }
+
+    cout<<ap;
+  
+}
